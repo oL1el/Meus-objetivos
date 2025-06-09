fe6abbaf439c716008787ef38658f61b208117e0
oLiel <160489849+oL1el@users.noreply.github.com>
2025-06-09 09:02:37 -0300

    .

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ for (let i = 0; i < botoes.length; i++) {
     botoes[i].classList.add("ativo");
   };
 }
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date ();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor (tempoFinal /1000);
+  let minutos = Math.floor (segundos/ 60);
+  let horas = Math.floor (minutos/ 60);
+  let dias = Math.floor (horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  return dias + " dias " + horas + " horas " + minutos + "minutos" + segundos + " segundos ";
+}
